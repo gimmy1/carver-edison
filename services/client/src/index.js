dd2a9a6b8aab925e14ef73934281fe88387c8caa
GAMAL ALI <gamalali@GAMALs-MacBook-Pro.local>
2019-01-11 12:43:19 -0500

    get request displayed in tabular form - react refactor with componentDidMount

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -5,11 +5,16 @@ import axios from 'axios';
 class App extends Component {
     constructor() {
       super();
-      this.getTweets();
+      this.state = {
+        tweets: [],
+      }
+    }
+    componentDidMount() {
+      this.getTweets()
     }
     getTweets() {
         axios.get(`${process.env.REACT_APP_TWEETS_SERVICE_URL}/tweets`)
-        .then((res) => { console.log(res); })
+        .then((res) => { this.setState({tweets:res.data.tweets}) })
         .catch((err) => { console.log(err); });
       }
     render() {
@@ -21,6 +26,17 @@ class App extends Component {
                 <br/>
                 <h1 className="title is-1">All Users</h1>
                 <hr/><br/>
+                {
+                  this.state.tweets.map((tweet) => {
+                    return (
+                      <h4
+                        key={tweet.id}
+                        className="box title is-4"
+                      >{ tweet.name }
+                      </h4>
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
